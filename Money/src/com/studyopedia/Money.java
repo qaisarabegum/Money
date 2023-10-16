1f14f3cb69882a76492720365fd2c63839e6366f
user <user@10.107.30.180>
2023-10-16 19:12:21 +0800

    First Commit

---
+++ Money/src/com/studyopedia/Money.java
@@ -0,0 +1,15 @@
+package com.studyopedia;
+
+public class Money {
+
+		    public static void main(String[] args) {
+		        // Display a title and welcome message in an attractive layout
+		        System.out.println("**********************************************");
+		        System.out.println("*        Making Money Bank ATM Service       *");
+		        System.out.println("**********************************************");
+		        System.out.println("                   Welcome!                  ");
+		    }
+		   
+
+	}
+
